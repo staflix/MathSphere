d7eaee21374ba3cf3d44abc94324dc5c5f9983b9
staflix <149697003+staflix@users.noreply.github.com>
2024-05-02 01:41:33 +0700

    ничего нового. Тоже самое, но с нормальной структурой

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # НУЖНО УСТАНОВИТЬ requirements.txt командой 'pip install -r requirements.txt'
-from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_login import LoginManager, login_required, logout_user
 from flask import Flask, render_template, redirect
-from forms.loginForm import LoginForm
-from forms.registerForm import RegisterForm
-from forms.simplearithmeticForm import SimpleArithmeticForm
 from data import db_session
-from sqlalchemy import select
 from data.users import User
-import random
+from api import registerAPI, loginAPI, simplearithmeticAPI
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -15,15 +11,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-flag, status, num1, num2, example, answer = True, None, None, None, None, None
-
 
 # для верной работы flask-login
 @login_manager.user_loader
 def load_user(user_id):
     db_session.global_init("db/MathSphereBase.db")
     db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
+    return db_sess.get(User, user_id)
 
 
 # выход с аккаунта, когда нажимаешь на имя
@@ -40,78 +34,18 @@ def index():
     return render_template('index.html')
 
 
-# регистрация с сохранением в бд, проверку на почту, проверку на совпадения пароля и его подтверждения
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.submit.data:
-        user = User()
-        user.surname = form.surname.data
-        user.name = form.name.data
-        user.surname = form.surname.data
-        user.profile_level = 1
-        user.email = form.email.data
-        user.set_password(form.password.data)
-        db_session.global_init("db/MathSphereBase.db")
-        db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='MathSphere', form=form)
-
-
-# авторизация, если в бд есть пользователь с данной почтой и пароли совпадают
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.submit.data:
-        db_session.global_init("db/MathSphereBase.db")
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
 # выбор математичесих методов
 @app.route('/trainer', methods=['GET', 'POST'])
 def trainer():
     return render_template('trainer.html')
 
 
-@app.route('/simple_arithmetic', methods=['GET', 'POST'])
-def simple_arithmetic():
-    # добавить уровни
-    global flag, status, num1, num2, example, answer
-    form = SimpleArithmeticForm()
-    if flag:
-        num1 = str(random.randint(1, 10))
-        num2 = str(random.randint(1, 10))
-        example = str(num1) + '+' + str(num2) + '=?'
-        answer = str(eval(f'{num1} + {num2}'))
-        status = 'Не решено'
-        flag = False
-    else:
-        if status == 'Решено верно!':
-            if form.next.data:
-                status = 'Не решено'
-                num1 = str(random.randint(1, 10))
-                num2 = str(random.randint(1, 10))
-                example = str(num1) + '+' + str(num2) + '=?'
-                answer = str(eval(f'{num1} + {num2}'))
-    if form.check.data:
-        if form.answer.data == answer:
-            status = 'Решено верно!'
-            return render_template('simple_arithmetic.html', example=example, status=status, form=form)
-        elif form.answer.data != answer:
-            status = 'Решено неверно!'
-            return render_template('simple_arithmetic.html', example=example, status=status, form=form)
-    return render_template('simple_arithmetic.html', example=example, status=status, form=form)
+def main():
+    app.register_blueprint(registerAPI.blueprint)
+    app.register_blueprint(loginAPI.blueprint)
+    app.register_blueprint(simplearithmeticAPI.blueprint)
+    app.run(port=5000, host='127.0.0.1', debug=True)
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1', debug=True)
+    main()
