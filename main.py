865a2940959b0f4d5ccc8228d14dd0b87a36c50f
staflix <staflix@yandex.ru>
2024-05-01 13:19:00 +0700

    Простой генератор для авторизованных

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from wtforms.validators import DataRequired
 from utils import make_session
 from sqlalchemy import select
 from DataBaseControl import User
+from math_techniques import MathTechniques
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+flag, status, num1, num2, example, answer = True, None, None, None, None, None
 
 
 # для верной работы flask-login
@@ -33,7 +37,7 @@ def logout():
 # главная страница
 @app.route('/')
 def index():
-    return render_template('index.html', title='MathSphere')
+    return render_template('index.html')
 
 
 # регистрация с сохранением в бд, проверку на почту, проверку на совпадения пароля и его подтверждения
@@ -87,6 +91,42 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+# выбор математичесих методов
+@app.route('/trainer', methods=['GET', 'POST'])
+def trainer():
+    return render_template('trainer.html')
+
+
+@app.route('/simple_arithmetic', methods=['GET', 'POST'])
+def simple_arithmetic():
+    # добавить уровни
+    global flag, status, num1, num2, example, answer
+    form = SimpleArithmeticForm()
+    if flag:
+        num1 = str(random.randint(1, 10))
+        num2 = str(random.randint(1, 10))
+        example = str(num1) + '+' + str(num2) + '=?'
+        answer = str(eval(f'{num1} + {num2}'))
+        status = 'Не решено'
+        flag = False
+    else:
+        if status == 'Решено верно!':
+            if form.next.data:
+                status = 'Не решено'
+                num1 = str(random.randint(1, 10))
+                num2 = str(random.randint(1, 10))
+                example = str(num1) + '+' + str(num2) + '=?'
+                answer = str(eval(f'{num1} + {num2}'))
+    if form.check.data:
+        if form.answer.data == answer:
+            status = 'Решено верно!'
+            return render_template('simple_arithmetic.html', example=example, status=status, form=form)
+        elif form.answer.data != answer:
+            status = 'Решено неверно!'
+            return render_template('simple_arithmetic.html', example=example, status=status, form=form)
+    return render_template('simple_arithmetic.html', example=example, status=status, form=form)
+
+
 # форма для логина
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
@@ -103,6 +143,25 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_confirmation = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Зарегестрироваться')
+
+
+# форма менюшки
+class MenuForm(FlaskForm):
+    trainer = SubmitField('Тренажер', validators=[DataRequired()])
+    # здесь добавить кнопочки меню
+
+
+# форма тренажера
+class TrainerForm(FlaskForm):
+    simple_arithmetic = SubmitField('Простая арифметика', validators=[DataRequired()])  # пример 23 + 43 =
+    # здесь добавить разные виды математических действий
+
+
+# форма для режима простая арифметика
+class SimpleArithmeticForm(FlaskForm):
+    answer = StringField('Ответить', validators=[DataRequired()])
+    check = SubmitField('Проверить')
+    next = SubmitField('Продолжить')
 
 
 if __name__ == '__main__':
