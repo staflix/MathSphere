f62820f2e45b22a8b7fdff8821dec0fb4db2a348
IceEnderGaming <72612462+F14my@users.noreply.github.com>
2024-05-01 12:25:45 +0700

    Login css
    +login page style/some changes in file structur

--- data/users.py
+++ data/users.py
@@ -1,8 +1,8 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from sqlalchemy.orm import declarative_base
 from sqlalchemy_serializer import SerializerMixin
 import sqlalchemy
-from utils import make_session
 
 SqlAlchemyBase = declarative_base()
 
@@ -18,22 +18,12 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
 
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
     def __repr__(self):
         return f"{self.id}-{self.surname}-{self.name}-{self.age}-" \
                f"{self.profile_level}-{self.email}-{self.hashed_password}"
-
-
-# создание объекта в таблицу
-session = make_session()
-
-# user = User()
-# user.surname = 'Nazarov'
-# user.name = 'Nazar'
-# user.profile_level = '1'
-# user.email = 'nazar_nazarov_84@list.ru'
-# user.hashed_password = 'zxc'
-
-# session.add(user)
-
-session.commit()
-session.close()
