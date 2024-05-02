72fc1b11a81052c4aa7ebedd71bde443f45596f2
staflix <149697003+staflix@users.noreply.github.com>
2024-05-02 20:04:34 +0700

    Classes and topics added.

---
+++ data/tools.py
@@ -0,0 +1,4 @@
+main_dictionary = {
+    'classes': {1: {'addition': 'Сложение', 'subtraction': 'Вычитание'},
+                2: {'multiplication': 'Умножение', 'division': 'Деление'},
+                3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {}, 10: {}, 11: {}}}
