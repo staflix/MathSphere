fa655e9068d34fe57d9966ba544ee6812ee7f2af
staflix <149697003+staflix@users.noreply.github.com>
2024-05-04 00:08:26 +0700

    Смена пароля. Небольшое изменение меню логина

--- api/loginAPI.py
+++ api/loginAPI.py
@@ -11,18 +11,44 @@ blueprint = flask.Blueprint(
     template_folder='templates'
 )
 
+message = 'почта'
+
 
 @blueprint.route('/login', methods=['GET', 'POST'])
 def login():
+    global message, email
     form = LoginForm()
-    if form.submit.data:
+
+    if form.input_password.data:
+
         db_session.global_init("db/MathSphereBase.db")
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
+        db_sess.close()
+
+        if user:
+            message = 'пароль'
+            email = form.email.data
+            return render_template('login.html', message_mode='пароль',
+                                   form=form, email=email)
+        else:
+            return render_template('login.html',
+                                   message="Аккаунта с такой почтой не существует!",
+                                   message_mode='пароль', form=form, email=email)
+    if form.submit.data:
+
+        db_session.global_init("db/MathSphereBase.db")
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == email).first()
+        db_sess.close()
+
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+
+        else:
+            render_template('login.html', form=form, message='Неправильный пароль',
+                            message_mode='пароль',
+                            email=email)
+
+    return render_template('login.html', form=form, message_mode='почта')
--- forms/loginForm.py
+++ forms/loginForm.py
@@ -6,5 +6,6 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()], render_kw={"placeholder": "Почта"})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"placeholder": "Пароль"})
+    input_password = SubmitField('Продолжить')
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
