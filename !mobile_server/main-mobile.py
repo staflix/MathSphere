c8c2b39b5dc1545ce37dbdbd10ded4addb6466ef
d1temnd <74726976+d1temnd@users.noreply.github.com>
2024-05-04 17:20:03 +0700

    add page / which get user.name

--- !mobile_server/main-mobile.py
+++ !mobile_server/main-mobile.py
@@ -6,14 +6,24 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+test_user = []
 
-def main():
-    db_session.global_init("../db/MathSphereBase.db")
+
+@app.route("/")
+def get_user_name():
     db_sess = db_session.create_session()
 
     for user in db_sess.query(User).all():
+        test_user.append(user.name)
         print(user.name)
-    # app.run()
+
+    return test_user
+
+
+def main():
+    db_session.global_init("../db/MathSphereBase.db")
+    app.run()
+
 
 
 if __name__ == '__main__':
