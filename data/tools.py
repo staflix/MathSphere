89ac7186888ff271a67d9aa381e624b371b747eb
SivetsDanil <103943389+SivetsDanil@users.noreply.github.com>
2024-05-07 22:44:17 +0700

    last version

--- data/tools.py
+++
@@ -1,4 +0,0 @@
-main_dictionary = {
-    'classes': {1: {'addition': 'Сложение', 'subtraction': 'Вычитание'},
-                2: {'multiplication': 'Умножение', 'division': 'Деление'},
-                3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {}, 10: {}, 11: {}}}
