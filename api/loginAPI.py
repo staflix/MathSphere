d463f207a510bdf2714b72827aaf7539e2deb4d6
staflix <149697003+staflix@users.noreply.github.com>
2024-05-11 14:13:24 +0700

    Super security. bgfix

--- api/loginAPI.py
+++ api/loginAPI.py
@@ -38,41 +38,41 @@ def login_email():
 @blueprint.route('/login/key=<rdm_string>', methods=['GET', 'POST'])
 def login_password(rdm_string):
     form = LoginForm()
+
     db_session.global_init("db/MathSphereBase.db")
     db_sess = db_session.create_session()
-    user_id = db_sess.query(Info).filter(Info.random_string == rdm_string).first().user_id
-    user = db_sess.query(User).filter(User.id == user_id).first()
+
+    user_info = db_sess.query(Info).filter(Info.random_string == rdm_string).first()
+    user = db_sess.query(User).filter(User.id == user_info.user_id).first()
     form.email.data = user.email
     db_sess.close()
-    form_new = LoginForm()
 
-    if form_new.validate_on_submit():
+    if form.validate_on_submit():
 
-        db_session.global_init("db/MathSphereBase.db")
         db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form_new.email.data).first()
-        db_sess.close()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
 
         if user:
-
             if user.check_password(form.password.data):
-                db_session.global_init("db/MathSphereBase.db")
-                db_sess = db_session.create_session()
-                user = db_sess.query(User).filter(User.email == form_new.email.data).first()
                 user_id = user.id
                 new_random_string = generate_string()
-                user_info = db_sess.query(Info).filter(Info.user_id == user_id).first()
-                user_info.random_string = new_random_string
+
+                db_sess.query(Info).filter(Info.user_id == user_id).update({'random_string': new_random_string})
                 db_sess.commit()
+
+                db_sess.refresh(user)
+
                 db_sess.close()
+
                 login_user(user, remember=form.remember_me.data)
                 return redirect(f'/key={new_random_string}')
-
             else:
-                return render_template('login_password.html', form=form_new, message='Неправильный пароль',
+                db_sess.close()
+                return render_template('login_password.html', form=form, message='Неправильный пароль',
                                        rdm_string=rdm_string)
         else:
-            return render_template('login_password.html', form=form_new, message='Такого аккаунта не существует',
+            db_sess.close()
+            return render_template('login_password.html', form=form, message='Такого аккаунта не существует',
                                    rdm_string=rdm_string)
 
     return render_template('login_password.html', form=form, rdm_string=rdm_string)
