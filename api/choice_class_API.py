0191d46d388f4c90898c761aa4060c0761831ed7
Flamy <72612462+F14my@users.noreply.github.com>
2024-05-31 19:48:06 +0700

    +company design 1/2

--- api/choice_class_API.py
+++ api/choice_class_API.py
@@ -1,6 +1,9 @@
 from flask import render_template, redirect
 import flask
 from forms.choice_class_Form import ChoiceClassForm
+from forms.mainpageForm import LogMainPageForm
+from data import db_session
+from data.users import User, Info
 
 blueprint = flask.Blueprint(
     'choiceclass_api',
@@ -12,7 +15,17 @@ blueprint = flask.Blueprint(
 @blueprint.route('/choice_class/key=<rdm_string>', methods=['GET', 'POST'])
 def choice_class(rdm_string):
     form = ChoiceClassForm()
+    profile = LogMainPageForm()
 
+    db_session.global_init("db/MathSphereBase.db")
+    db_sess = db_session.create_session()
+
+    user_info = db_sess.query(Info).filter(Info.random_string == rdm_string).first()
+    user = db_sess.query(User).filter(User.id == user_info.user_id).first()
+    user_name = user.name
+    user_surname = user.surname
+    user_email = user.email
+    user_avatar = f"../{user_info.avatar_href}"
     if form.first_class.data:
         return redirect(f"/1/key={rdm_string}")
 
@@ -25,4 +38,6 @@ def choice_class(rdm_string):
     elif form.fourth_class.data:
         return redirect(f"/4/key={rdm_string}")
 
-    return render_template("choice_class.html", form=form, rdm_string=rdm_string)
+    return render_template("choice_class.html", form=form, rdm_string=rdm_string, profile=profile,
+                           avatar=user_avatar, name=user_name, surname=user_surname,
+                           email=user_email)
