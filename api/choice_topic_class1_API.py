4bfc8b9f2fd3d8134a42b5ae5b0e257c42ba6ab2
Flamy <72612462+F14my@users.noreply.github.com>
2024-05-31 23:47:46 +0700

    trainer/policy designs

--- api/choice_topic_class1_API.py
+++ api/choice_topic_class1_API.py
@@ -1,6 +1,9 @@
 from flask import render_template, redirect
 import flask
 from forms.choice_topic_class1_Form import ChoiceTopicClass1Form
+from forms.mainpageForm import LogMainPageForm
+from data import db_session
+from data.users import User, Info
 
 blueprint = flask.Blueprint(
     'choicetopicclass1_api',
@@ -12,9 +15,22 @@ blueprint = flask.Blueprint(
 @blueprint.route('/<int:num_class>/key=<rdm_string>', methods=['GET', 'POST'])
 def choice_topic_class1(num_class, rdm_string):
     form = ChoiceTopicClass1Form()
+    profile = LogMainPageForm()
 
-    if form.item_count_etc.data:
+    db_session.global_init("db/MathSphereBase.db")
+    db_sess = db_session.create_session()
+
+    user_info = db_sess.query(Info).filter(Info.random_string == rdm_string).first()
+    user = db_sess.query(User).filter(User.id == user_info.user_id).first()
+    user_name = user.name
+    user_surname = user.surname
+    user_email = user.email
+    user_avatar = f"../{user_info.avatar_href}"
+
+    if form.topic1.data:
         topic = "Счет предметов. Сравнение групп предметов. Пространственные и временные представления."
         return redirect(f"/{num_class}/{topic}/key={rdm_string}")
 
-    return render_template("choice_topic.html", num_class=num_class, form=form, rdm_string=rdm_string)
+    return render_template("choice_topic.html", num_class=num_class, form=form, rdm_string=rdm_string, profile=profile,
+                           avatar=user_avatar, name=user_name, surname=user_surname,
+                           email=user_email)
