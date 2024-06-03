2adffc4f6a67ccbffc59c2d85b15cef7fb1cd535
staflix <149697003+staflix@users.noreply.github.com>
2024-06-03 22:32:41 +0700

    bg fix

--- forms/choice_topic_all_classes_Form.py
+++ forms/choice_topic_all_classes_Form.py
@@ -3,12 +3,12 @@ from wtforms import SubmitField
 
 
 class ChoiceTopicAllClassesForm(FlaskForm):
-    topic1 = SubmitField(
+    topic11 = SubmitField(
         'Счет предметов. Сравнение групп предметов. Пространственные и временные представления.')
-    topic2 = SubmitField('Тема2')
-    topic3 = SubmitField('Тема3')
-    topic4 = SubmitField('Тема4')
-    topic5 = SubmitField('Тема5')
-    topic6 = SubmitField('Тема6')
-    topic7 = SubmitField('Тема7')
-    topic8 = SubmitField('Тема8')
+    topic12 = SubmitField('Многоугольники')
+    topic13 = SubmitField('Задачки на увеличение')
+    topic14 = SubmitField('Задачки на уменьшение')
+    topic15 = SubmitField('Задачки (разнобой)')
+    topic16 = SubmitField('Примеры на счет')
+    topic17 = SubmitField('Тима тома ни разлий вода!+!+_"_"')
+    topic18 = SubmitField('BANKAII КАНУМБИРАЧИ ИНЫШМЕ АРАТЕМУ ТУДУДУДУ ТУДУДУДУ')
--- forms/simplearithmeticForm.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class SimpleArithmeticForm(FlaskForm):
-    answer = StringField('Ответить', validators=[DataRequired()])
-    check = SubmitField('Проверить')
-    next = SubmitField('Продолжить')
